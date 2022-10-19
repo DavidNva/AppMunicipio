5b54af349f7cc7e53cd266ec1e8fa57b481a04ae
David Nava <david.nava.garcia4@gmail.com>
2022-10-19 08:29:58 -0500

    Aplicacion municipio: Consultar, Insertar, Actualizar, Eliminar de manera básica

---
+++ AppMunicipio/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace AppMunicipio
+{
+    partial class Frm_Municipio
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvMunicipio = new System.Windows.Forms.DataGridView();
+            this.btnConsultarMun = new System.Windows.Forms.Button();
+            this.btnInsertarMun = new System.Windows.Forms.Button();
+            this.txtIDMunicipio = new System.Windows.Forms.TextBox();
+            this.txtNombreMunicipio = new System.Windows.Forms.TextBox();
+            this.txtCodigoPostal = new System.Windows.Forms.TextBox();
+            this.txtDireccion = new System.Windows.Forms.TextBox();
+            this.lblIDMunicipio = new System.Windows.Forms.Label();
+            this.lblNombreMunicipio = new System.Windows.Forms.Label();
+            this.lblDireccionMun = new System.Windows.Forms.Label();
+            this.lblCodigoPostalMun = new System.Windows.Forms.Label();
+            this.btnEliminarMun = new System.Windows.Forms.Button();
+            this.btnActualizarMun = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMunicipio)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvMunicipio
+            // 
+            this.dgvMunicipio.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMunicipio.Location = new System.Drawing.Point(46, 224);
+            this.dgvMunicipio.Name = "dgvMunicipio";
+            this.dgvMunicipio.Size = new System.Drawing.Size(467, 150);
+            this.dgvMunicipio.TabIndex = 0;
+            // 
+            // btnConsultarMun
+            // 
+            this.btnConsultarMun.Location = new System.Drawing.Point(63, 154);
+            this.btnConsultarMun.Name = "btnConsultarMun";
+            this.btnConsultarMun.Size = new System.Drawing.Size(75, 23);
+            this.btnConsultarMun.TabIndex = 1;
+            this.btnConsultarMun.Text = "Consultar";
+            this.btnConsultarMun.UseVisualStyleBackColor = true;
+            this.btnConsultarMun.Click += new System.EventHandler(this.btnConsultarMun_Click);
+            // 
+            // btnInsertarMun
+            // 
+            this.btnInsertarMun.Location = new System.Drawing.Point(178, 154);
+            this.btnInsertarMun.Name = "btnInsertarMun";
+            this.btnInsertarMun.Size = new System.Drawing.Size(75, 23);
+            this.btnInsertarMun.TabIndex = 2;
+            this.btnInsertarMun.Text = "Insertar";
+            this.btnInsertarMun.UseVisualStyleBackColor = true;
+            this.btnInsertarMun.Click += new System.EventHandler(this.btnInsertarMun_Click);
+            // 
+            // txtIDMunicipio
+            // 
+            this.txtIDMunicipio.Location = new System.Drawing.Point(178, 16);
+            this.txtIDMunicipio.Name = "txtIDMunicipio";
+            this.txtIDMunicipio.Size = new System.Drawing.Size(335, 20);
+            this.txtIDMunicipio.TabIndex = 3;
+            // 
+            // txtNombreMunicipio
+            // 
+            this.txtNombreMunicipio.Location = new System.Drawing.Point(178, 50);
+            this.txtNombreMunicipio.Name = "txtNombreMunicipio";
+            this.txtNombreMunicipio.Size = new System.Drawing.Size(335, 20);
+            this.txtNombreMunicipio.TabIndex = 4;
+            // 
+            // txtCodigoPostal
+            // 
+            this.txtCodigoPostal.Location = new System.Drawing.Point(178, 116);
+            this.txtCodigoPostal.Name = "txtCodigoPostal";
+            this.txtCodigoPostal.Size = new System.Drawing.Size(335, 20);
+            this.txtCodigoPostal.TabIndex = 6;
+            // 
+            // txtDireccion
+            // 
+            this.txtDireccion.Location = new System.Drawing.Point(178, 79);
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.Size = new System.Drawing.Size(335, 20);
+            this.txtDireccion.TabIndex = 5;
+            // 
+            // lblIDMunicipio
+            // 
+            this.lblIDMunicipio.AutoSize = true;
+            this.lblIDMunicipio.Location = new System.Drawing.Point(43, 22);
+            this.lblIDMunicipio.Name = "lblIDMunicipio";
+            this.lblIDMunicipio.Size = new System.Drawing.Size(64, 13);
+            this.lblIDMunicipio.TabIndex = 7;
+            this.lblIDMunicipio.Text = "IdMunicipio:";
+            // 
+            // lblNombreMunicipio
+            // 
+            this.lblNombreMunicipio.AutoSize = true;
+            this.lblNombreMunicipio.Location = new System.Drawing.Point(43, 57);
+            this.lblNombreMunicipio.Name = "lblNombreMunicipio";
+            this.lblNombreMunicipio.Size = new System.Drawing.Size(95, 13);
+            this.lblNombreMunicipio.TabIndex = 8;
+            this.lblNombreMunicipio.Text = "Nombre Municipio:";
+            // 
+            // lblDireccionMun
+            // 
+            this.lblDireccionMun.AutoSize = true;
+            this.lblDireccionMun.Location = new System.Drawing.Point(43, 86);
+            this.lblDireccionMun.Name = "lblDireccionMun";
+            this.lblDireccionMun.Size = new System.Drawing.Size(55, 13);
+            this.lblDireccionMun.TabIndex = 9;
+            this.lblDireccionMun.Text = "Dirección:";
+            // 
+            // lblCodigoPostalMun
+            // 
+            this.lblCodigoPostalMun.AutoSize = true;
+            this.lblCodigoPostalMun.Location = new System.Drawing.Point(43, 119);
+            this.lblCodigoPostalMun.Name = "lblCodigoPostalMun";
+            this.lblCodigoPostalMun.Size = new System.Drawing.Size(78, 13);
+            this.lblCodigoPostalMun.TabIndex = 10;
+            this.lblCodigoPostalMun.Text = "CódigoPostal;>";
+            // 
+            // btnEliminarMun
+            // 
+            this.btnEliminarMun.Location = new System.Drawing.Point(424, 154);
+            this.btnEliminarMun.Name = "btnEliminarMun";
+            this.btnEliminarMun.Size = new System.Drawing.Size(89, 23);
+            this.btnEliminarMun.TabIndex = 11;
+            this.btnEliminarMun.Text = "Eliminar";
+            this.btnEliminarMun.UseVisualStyleBackColor = true;
+            this.btnEliminarMun.Click += new System.EventHandler(this.btnEliminarMun_Click);
+            // 
+            // btnActualizarMun
+            // 
+            this.btnActualizarMun.Location = new System.Drawing.Point(287, 154);
+            this.btnActualizarMun.Name = "btnActualizarMun";
+            this.btnActualizarMun.Size = new System.Drawing.Size(87, 23);
+            this.btnActualizarMun.TabIndex = 12;
+            this.btnActualizarMun.Text = "Actualizar";
+            this.btnActualizarMun.UseVisualStyleBackColor = true;
+            this.btnActualizarMun.Click += new System.EventHandler(this.btnActualizarMun_Click);
+            // 
+            // Frm_Municipio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(561, 442);
+            this.Controls.Add(this.btnActualizarMun);
+            this.Controls.Add(this.btnEliminarMun);
+            this.Controls.Add(this.lblCodigoPostalMun);
+            this.Controls.Add(this.lblDireccionMun);
+            this.Controls.Add(this.lblNombreMunicipio);
+            this.Controls.Add(this.lblIDMunicipio);
+            this.Controls.Add(this.txtCodigoPostal);
+            this.Controls.Add(this.txtDireccion);
+            this.Controls.Add(this.txtNombreMunicipio);
+            this.Controls.Add(this.txtIDMunicipio);
+            this.Controls.Add(this.btnInsertarMun);
+            this.Controls.Add(this.btnConsultarMun);
+            this.Controls.Add(this.dgvMunicipio);
+            this.Name = "Frm_Municipio";
+            this.Text = "Municipios";
+            this.Load += new System.EventHandler(this.Frm_Municipio_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMunicipio)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvMunicipio;
+        private System.Windows.Forms.Button btnConsultarMun;
+        private System.Windows.Forms.Button btnInsertarMun;
+        private System.Windows.Forms.TextBox txtIDMunicipio;
+        private System.Windows.Forms.TextBox txtNombreMunicipio;
+        private System.Windows.Forms.TextBox txtCodigoPostal;
+        private System.Windows.Forms.TextBox txtDireccion;
+        private System.Windows.Forms.Label lblIDMunicipio;
+        private System.Windows.Forms.Label lblNombreMunicipio;
+        private System.Windows.Forms.Label lblDireccionMun;
+        private System.Windows.Forms.Label lblCodigoPostalMun;
+        private System.Windows.Forms.Button btnEliminarMun;
+        private System.Windows.Forms.Button btnActualizarMun;
+    }
+}
+
---
+++ AppMunicipio/Form1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace AppMunicipio
+{
+    public partial class Frm_Municipio : Form
+    {
+        SqlConnection cnMunicipio;
+        SqlCommand cmdMunicipio;
+        SqlParameter  paMunicipio;
+        DataSet dsMunicipio;
+        SqlDataAdapter daMunicipio;
+
+        public Frm_Municipio()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Frm_Municipio_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConsultarMun_Click(object sender, EventArgs e)
+        {
+            cnMunicipio = new SqlConnection();
+            cnMunicipio.ConnectionString = Program.cadenaMunicipio;
+            cnMunicipio.Open();
+            if(dsMunicipio == null)
+            {
+                dsMunicipio = new DataSet();
+
+            }
+            if(daMunicipio == null)
+            {
+                daMunicipio = new SqlDataAdapter("SELECT * FROM Municipio",cnMunicipio);
+                dsMunicipio.Clear();//Limpia el data set
+                daMunicipio.Fill(dsMunicipio,"Municipio");
+                dgvMunicipio.DataSource = dsMunicipio;
+                dgvMunicipio.DataMember = "Municipio";
+                dsMunicipio = null;
+                daMunicipio = null;
+                cnMunicipio.Close();
+            }
+        }
+
+        private void btnInsertarMun_Click(object sender, EventArgs e)
+        {
+            try { 
+            cnMunicipio = new SqlConnection();
+            cnMunicipio.ConnectionString = Program.cadenaMunicipio;
+            cnMunicipio.Open();
+            cmdMunicipio = new SqlCommand();
+            cmdMunicipio.CommandText = "sp_Insert_Municipio";
+            cmdMunicipio.CommandType = CommandType.StoredProcedure;
+            cmdMunicipio.Parameters.Clear();
+
+            paMunicipio = new SqlParameter();
+            paMunicipio.ParameterName = "@IDMunicipio";
+            paMunicipio.SqlDbType = SqlDbType.Int;
+            paMunicipio.Size = 4;
+            paMunicipio.Value = Convert.ToInt32(txtIDMunicipio.Text);
+            paMunicipio.Direction = ParameterDirection.Input;//Porque es de dato de entrada
+            cmdMunicipio.Parameters.Add(paMunicipio);
+
+            paMunicipio = new SqlParameter();
+            paMunicipio.ParameterName = "@NombreMunicipio";
+            paMunicipio.SqlDbType = SqlDbType.Char;
+            paMunicipio.Size = 50;
+            paMunicipio.Value = txtNombreMunicipio.Text;
+            paMunicipio.Direction = ParameterDirection.Input;//Porque es de dato de entrada
+            cmdMunicipio.Parameters.Add(paMunicipio);
+
+            paMunicipio = new SqlParameter();
+            paMunicipio.ParameterName = "@Direccion";
+            paMunicipio.SqlDbType = SqlDbType.Char;
+            paMunicipio.Size = 50;
+            paMunicipio.Value = txtDireccion.Text;
+            paMunicipio.Direction = ParameterDirection.Input;//Porque es de dato de entrada
+            cmdMunicipio.Parameters.Add(paMunicipio);
+
+            paMunicipio = new SqlParameter();
+            paMunicipio.ParameterName = "@CodPostalMun";
+            paMunicipio.SqlDbType = SqlDbType.Char;
+            paMunicipio.Size = 50;
+            paMunicipio.Value = txtCodigoPostal.Text;
+            paMunicipio.Direction = ParameterDirection.Input;//Porque es de dato de entrada
+            cmdMunicipio.Parameters.Add(paMunicipio);
+
+            
+
+            cmdMunicipio.Connection = cnMunicipio;
+
+            int i = cmdMunicipio.ExecuteNonQuery();
+
+            if (i != 0)
+            {
+
+                MessageBox.Show("Datos ingresados correctamente", "Insertar Municipio", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                
+            }
+            else
+            {
+                MessageBox.Show("Corriga sus datos","Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+            }   
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error: "+ ex.Message, "Error a la insertar",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+            finally
+            {
+                cnMunicipio.Close();
+            }
+             
+        }
+
+        private void btnActualizarMun_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                cnMunicipio = new SqlConnection();
+                cnMunicipio.ConnectionString = Program.cadenaMunicipio;
+                cnMunicipio.Open();
+                cmdMunicipio = new SqlCommand();
+                cmdMunicipio.CommandText = "sp_Update_Municipio";
+                cmdMunicipio.CommandType = CommandType.StoredProcedure;
+                cmdMunicipio.Parameters.Clear();
+
+                paMunicipio = new SqlParameter();
+                paMunicipio.ParameterName = "@IDMunicipio";
+                paMunicipio.SqlDbType = SqlDbType.Int;
+                paMunicipio.Size = 4;
+                paMunicipio.Value = Convert.ToInt32(txtIDMunicipio.Text);
+                paMunicipio.Direction = ParameterDirection.Input;//Porque es de dato de entrada
+                cmdMunicipio.Parameters.Add(paMunicipio);
+
+                paMunicipio = new SqlParameter();
+                paMunicipio.ParameterName = "@NombreMunicipio";
+                paMunicipio.SqlDbType = SqlDbType.Char;
+                paMunicipio.Size = 50;
+                paMunicipio.Value = txtNombreMunicipio.Text;
+                paMunicipio.Direction = ParameterDirection.Input;//Porque es de dato de entrada
+                cmdMunicipio.Parameters.Add(paMunicipio);
+
+                paMunicipio = new SqlParameter();
+                paMunicipio.ParameterName = "@Direccion";
+                paMunicipio.SqlDbType = SqlDbType.Char;
+                paMunicipio.Size = 50;
+                paMunicipio.Value = txtDireccion.Text;
+                paMunicipio.Direction = ParameterDirection.Input;//Porque es de dato de entrada
+                cmdMunicipio.Parameters.Add(paMunicipio);
+
+                paMunicipio = new SqlParameter();
+                paMunicipio.ParameterName = "@CodPostalMun";
+                paMunicipio.SqlDbType = SqlDbType.Char;
+                paMunicipio.Size = 50;
+                paMunicipio.Value = txtCodigoPostal.Text;
+                paMunicipio.Direction = ParameterDirection.Input;//Porque es de dato de entrada
+                cmdMunicipio.Parameters.Add(paMunicipio);
+
+
+
+                cmdMunicipio.Connection = cnMunicipio;
+
+                int i = cmdMunicipio.ExecuteNonQuery();
+
+                if (i != 0)
+                {
+
+                    MessageBox.Show("Datos actualizados correctamente", "Actualizados Municipio", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                }
+                else
+                {
+                    MessageBox.Show("Corriga sus datos", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Error a la actualizar", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                cnMunicipio.Close();
+            }
+        }
+
+        private void btnEliminarMun_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                cnMunicipio = new SqlConnection();
+                cnMunicipio.ConnectionString = Program.cadenaMunicipio;
+                cnMunicipio.Open();
+                cmdMunicipio = new SqlCommand();
+                cmdMunicipio.CommandText = "sp_Delete_Municipio";
+                cmdMunicipio.CommandType = CommandType.StoredProcedure;
+                cmdMunicipio.Parameters.Clear();
+
+                paMunicipio = new SqlParameter();
+                paMunicipio.ParameterName = "@IDMunicipio";
+                paMunicipio.SqlDbType = SqlDbType.Int;
+                paMunicipio.Size = 4;
+                paMunicipio.Value = Convert.ToInt32(txtIDMunicipio.Text);
+                paMunicipio.Direction = ParameterDirection.Input;//Porque es de dato de entrada
+                cmdMunicipio.Parameters.Add(paMunicipio);
+
+                cmdMunicipio.Connection = cnMunicipio;
+
+                int i = cmdMunicipio.ExecuteNonQuery();
+
+                if (i != 0)
+                {
+
+                    MessageBox.Show("El dato se eliminó correctamente", "Eliminar Municipio", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                }
+                else
+                {
+                    MessageBox.Show("Corriga sus datos", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Error a la eliminar", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                cnMunicipio.Close();
+            }
+        }
+    }
+}
